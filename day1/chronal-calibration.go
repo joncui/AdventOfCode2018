3c20b6f9474ab5fe282e0ab91ef166346c930bff
Jonathan Cui <joncui1101@gmail.com>
2018-12-01 16:10:31 -0800

    Day1 Complete

---
+++ day1/chronal-calibration.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func main() {
+	file := os.Args[1]
+	fmt.Printf(file)
+
+	rawData, err := ioutil.ReadFile("data")
+	check(err)
+
+	frequencyChanges := strings.Split(strings.TrimSuffix(string(rawData), "\n"), "\n")
+	var intFrequencyChanges []int
+	frequency := 0
+
+	for _, frequencyChange := range frequencyChanges {
+		intFreqChange, err := strconv.Atoi(frequencyChange)
+		check(err)
+
+		intFrequencyChanges = append(intFrequencyChanges, intFreqChange)
+
+		frequency += intFreqChange
+	}
+
+	fmt.Printf("The resulting frequency is %d.\n", frequency)
+	fmt.Printf("The first frequency the device reaches twice is: %d.\n", getPart2Solution(intFrequencyChanges))
+}
+
+func getPart2Solution(intFrequencyChanges []int) int {
+
+	frequency := 0
+	frequencyMap := make(map[int]bool)
+	frequencyMap[frequency] = false
+
+	for i := 0; true; i++ {
+		frequency += intFrequencyChanges[i]
+
+		value, exists := frequencyMap[frequency]
+		if !exists {
+			frequencyMap[frequency] = false
+		} else if exists && !value {
+			frequencyMap[frequency] = true
+			break
+		}
+
+		if len(intFrequencyChanges) == (i + 1) {
+			i = -1
+		}
+	}
+
+	return frequency
+}
