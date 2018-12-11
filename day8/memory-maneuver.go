6cd823900b91958bc48eb7f62712bd901ce70b26
Jonathan Cui <joncui1101@gmail.com>
2018-12-11 10:14:26 -0800

    Complete day8

--- day8/memory-maneuver.go
+++ day8/memory-maneuver.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 )
 
+var headerSize = 2
+
 func check(e error) {
 	if e != nil {
 		log.Fatal(e)
@@ -24,35 +26,70 @@ func parseData() []string {
 	rawData, err := ioutil.ReadFile(file)
 	check(err)
 
-	return strings.Split(strings.TrimSuffix(string(rawData), " "), "\n")
+	return strings.Split(strings.TrimSuffix(string(rawData), "\n"), " ")
 }
 
-func main() {
-	nums := parseData()
-	// fmt.Println(nums)
-	fmt.Println("vim go")
-
-	licenseFileCheck := 0
-	stack := []int{}
-	for i := 0; i < len(nums); {
-		childNodeNum := strToNum(nums[i])
-		metaDataNum := strToNum(nums[i+1])
-
-		if childNodeNum == 0 {
-			for _, metaData := range nums[i+2 : i+2+metaDataNum] {
-				licenseFileCheck += strToNum(metaData)
-			}
-
-			i += (2 + metaDataNum)
-		} else {
-			stack = append(stack, metaDataNum)
-		}
-	}
-}
-
-func strToNum(str string) int {
+func strToInt(str string) int {
 	num, err := strconv.Atoi(str)
 	check(err)
 
 	return num
+}
+
+func transformDataToIntArr(stringData []string) (intData []int) {
+	for _, str := range stringData {
+		intData = append(intData, strToInt(str))
+	}
+
+	return
+}
+
+func sumArr(arr []int) (sum int) {
+	for _, num := range arr {
+		sum += num
+	}
+
+	return
+}
+
+func main() {
+	solve(transformDataToIntArr(parseData()))
+}
+
+func solve(nums []int) {
+	total, values, _ := postOrderTraversal(nums)
+
+	fmt.Printf("The sum of all metadata entries is %d\n", total)
+	fmt.Printf("The value of the root node is %d\n", values)
+}
+
+func postOrderTraversal(data []int) (int, int, []int) {
+	childNodeNum := data[0]
+	metaDataNum := data[1]
+	data = data[2:]
+	values := []int{}
+	totals := 0
+
+	for i := 0; i < childNodeNum; i++ {
+		var total int
+		var value int
+		total, value, data = postOrderTraversal(data)
+		totals += total
+		values = append(values, value)
+	}
+
+	totals += sumArr(data[:metaDataNum])
+
+	if childNodeNum == 0 {
+		return totals, sumArr(data[:metaDataNum]), data[metaDataNum:]
+	} else {
+		sumValues := 0
+		for _, metadata := range data[:metaDataNum] {
+			if metadata > 0 && metadata <= len(values) {
+				sumValues += values[metadata-1]
+			}
+		}
+
+		return totals, sumValues, data[metaDataNum:]
+	}
 }
